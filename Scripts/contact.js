417b5338d9abf68fc3e3988274287426c6abd7f1
csalgado <csalgado@companallc.com>
2024-04-26 16:41:47 -0700

    Assignment 2.3

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -1,79 +1,144 @@
-//const submitButton = document.getElementById("contactForm");
-//submitButton.addEventListener('click', validateData);
+//SETUP
 
-function validateData(){
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let phone = document.getElementById('phone').value;
-    let message = document.getElementById('message').value;
-    let topic = document.getElementById('topic').value;
+class contactForm{    
 
-    //Console Log
-    console.log('Test Email: ', email, ' = ', validateEmail(email));
-    console.log('Test Phone: ', phone, ' = ', validatePhone(phone));    
+    constructor(name, email, phone, message){
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.message = message;        
+    }
 
-    //Validate Email
-    if(email != null && validateEmail(email)){
-        //Validate Phone
-        if(phone != null && validatePhone(phone)){
-            //Validate Topic
-            let responseMessage = getResponseForTopic(topic);
-            if(topic === 'none'){
-                alert(responseMessage)
+    instantiateForm(){
+        const nameInput = document.getElementById('name');
+        const emailInput = document.getElementById('email');
+        const phoneInput = document.getElementById('phone');
+        const messageInput = document.getElementById('message');
+
+        nameInput.value = this.name;
+        emailInput.value = this.email;
+        phoneInput.value = this.phone;
+        messageInput.value = this.message;
+    }
+
+    validateForm(){        
+        let tempName = document.getElementById('name').value;
+        let tempEmail = document.getElementById('email').value;
+        let tempPhone = document.getElementById('phone').value;
+        let tempMessage = document.getElementById('message').value;
+        let tempTopic = document.getElementById('topic').value;
+        let tempLocation = document.getElementById('location').value;
+
+        //Console Log
+        console.log('Test Email Validate: ', tempEmail, ' = ', this.validateEmail(tempEmail));
+        console.log('Test Phone Validate: ', tempPhone, ' = ', this.validatePhone(tempPhone));
+
+        //Validate Email
+        if(tempEmail != null && this.validateEmail(tempEmail)){
+            //Validate Phone
+            if(tempPhone != null && this.validatePhone(tempPhone)){
+                //Validate Topic
+                let responseMessage = this.getResponseForTopic(tempTopic);
+                if(tempTopic === 'none'){
+                    alert(responseMessage)
+                }
+                else{                            
+                    this.setForm(tempName, tempEmail, tempPhone, tempMessage, tempTopic, tempLocation);
+                    this.displayForm(responseMessage);
+                }
             }
-            else{                            
-                alert('Thank you ' + name + '!\nEmail: ' + email +
-                '\nPhone: ' + phone + '\nMessage: ' + message +
-                '\nTopic: ' + topic + '\n' + responseMessage);
+            else{
+                alert('Phone is not valid');
             }
         }
         else{
-            alert('Phone is not valid');
+            alert('Email is not valid!');
+        }        
+    }
+
+    validateEmail(email){
+        return email.includes('@');
+    }
+    
+    validatePhone(phone){  
+        return phone.length >= 7;
+    }
+
+    getResponseForTopic(topic){
+        console.log('Test Topic: ', topic);
+        let responseMessage = '';
+    
+        switch (topic){
+            case 'none':
+                responseMessage = 'Please choose a topic!';
+                break;
+            case 'question':
+                responseMessage = 'Thank you for your Question!';
+                break;
+            case 'answer':
+                responseMessage = 'Thank you for your Answer!';
+                break;
+            case 'issue':
+                responseMessage = 'We will review your Issue!';
+                break;
+            case 'request':
+                responseMessage = 'We will review your Request!';
+                break;
+            case 'recommendation':
+                responseMessage = 'Thank you for your Recommendation!';
+                break;
+            default:
+                responseMessage = 'Thank you for your visiting!'
         }
-    }
-    else{
-        alert('Email is not valid!');
-    }
-}
-
-function validateEmail(email){
-    //const regex = /@/;
-    //return regex.test(email);
-    return email.includes('@');
-}
-
-function validatePhone(phone){
-    //const regex = /\d/;
-    //return regex.test(phone);    
-    return phone.length >= 7;
-}
-
-function getResponseForTopic(topic){
-    console.log('Test Topic: ', topic);
-    let responseMessage = '';
-
-    switch (topic){
-        case 'none':
-            responseMessage = 'Please choose a topic!';
-            break;
-        case 'question':
-            responseMessage = 'Thank you for your Question!';
-            break;
-        case 'answer':
-            responseMessage = 'Thank you for your Answer!';
-            break;
-        case 'issue':
-            responseMessage = 'We will review your Issue!';
-            break;
-        case 'request':
-            responseMessage = 'We will review your Request!';
-            break;
-        case 'recommendation':
-            responseMessage = 'Thank you for your Recommendation!';
-            break;
-        default:
-            responseMessage = 'Thank you for your visiting!'
+    
+        return responseMessage;
     }
 
-    return responseMessage;
+    setForm(nameInput, emailInput, phoneInput, messageInput, topicInput, locationInput){
+        this.name = nameInput;
+        this.email = emailInput;
+        this.phone = phoneInput;
+        this.message = messageInput;
+        this.topic = topicInput;
+        this.location = locationInput;
+    }
+
+    displayForm(responseMessage){
+        alert('Thank you ' + this.name + '!\nEmail: ' + this.email +
+            '\nPhone: ' + this.phone + '\nLocation: ' + this.location + 
+            '\nMessage: ' + this.message + '\nTopic: ' + this.topic + '\n' + responseMessage);
+    }
+
 }
+
+const locationOptions = [
+    {value: "usa", text: "United States"},
+    {value:"can", text: "Canada"}, 
+    {value: "mex", text: "Mexico"},
+];
+
+const selectLocation = document.getElementById('location');
+
+function addOption(option){
+    console.log('Test option: ', option.value, ' and ', option.text);
+    const optionElement = document.createElement("option");
+    optionElement.value = option.value;
+    optionElement.textContent = option.text;
+    selectLocation.appendChild(optionElement);
+};
+
+window.onload = function(){
+    console.log('On Load');
+
+    locationOptions.forEach(option => addOption(option));
+
+    mainContactForm.instantiateForm();
+};
+
+function validateData(){
+    mainContactForm.validateForm();
+}
+
+//MAIN
+
+const mainContactForm = new contactForm("Chris Salgado", "chrisasalgado@aol.com", "7147120693", "Sample message");
