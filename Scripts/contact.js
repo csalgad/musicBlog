61434db5bb4132bb401927a6607d89ebd3c18455
Chris Salgado <chrisasalgado@gmail.com>
2024-05-05 08:08:39 -0700

    Assignment 2.3

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -1,6 +1,35 @@
 //const submitButton = document.getElementById("contactForm");
 //submitButton.addEventListener('click', validateData);
 
+class contactForm{
+    constructor(name, email, phone){
+        
+    }
+
+}
+
+const locationOptions = [
+    {value: "usa", text: "United States"},
+    {value:"can", text: "Canada"}, 
+    {value: "mex", text: "Mexico"},
+];
+
+const selectLocation = document.getElementById('location');
+
+function addOption(option){
+    console.log('Test option: ', option.value, ' and ', option.text);
+    const optionElement = document.createElement("option");
+    optionElement.value = option.value;
+    optionElement.textContent = option.text;
+    selectLocation.appendChild(optionElement);
+};
+
+window.onload = function(){
+    console.log('On Load');
+
+    locationOptions.forEach(option => addOption(option));
+};
+
 function validateData(){
     let name = document.getElementById('name');
     let email = document.getElementById('email');
@@ -23,14 +52,14 @@ function validateData(){
     else{
         alert('Email is not valid!');
     }
-}
+};
 
 function validateEmail(email){
     const regex = /@/;
     return regex.test(email);
-}
+};
 
 function validatePhone(phone){
     const regex = /\d/;
     return regex.test(phone);
-}
+};
